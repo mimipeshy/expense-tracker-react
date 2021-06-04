daa9bbd8023b095136040e76501a47b6a14d3762
mimipeshy <perisndanu@gmail.com>
2021-06-04 22:35:04 +0300

    Update Route component

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
 import ProtectedLogin from '../containers/ProtectedLogin';
+import ProtectedRoute from '../containers/ProtectedRoute';
+import Dashboard from '../containers/Dashboard';
 import Login from '../containers/Login';
 
 const Routes = () => (
   <Switch>
     <ProtectedLogin path="/login" component={Login} />
+    <ProtectedRoute path="/" component={Dashboard} />
   </Switch>
 );
 export default Routes;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,25 +4,25 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 function Home({ user }) {
-    return (
-        <div className="home-container">
-            <h1>Welcome {user.username.toUpperCase()}</h1>
-            <h3>
-                &quot;One of the best tests of a trader is what they do when the markets
-                are closed&quot; - Brett Steenbarger
+  return (
+    <div className="home-container">
+      <h1>Welcome {user.username.toUpperCase()}</h1>
+      <h3>
+        &quot;Beware of little expenses.A small leak will sink a great
+        ship&quot; - Benjamin Franklin
       </h3>
-        </div>
-    );
+    </div>
+  );
 }
 
 Home.propTypes = {
-    user: PropTypes.shape({
-        username: PropTypes.string,
-    }).isRequired,
+  user: PropTypes.shape({
+    username: PropTypes.string,
+  }).isRequired,
 };
 
-const mapStateToProps = state => ({
-    user: state.userReducer.user,
+const mapStateToProps = (state) => ({
+  user: state.userReducer.user,
 });
 
 const mapDispatchToProps = () => ({});
