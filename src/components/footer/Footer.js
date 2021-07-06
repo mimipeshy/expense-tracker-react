b349cebdf9852100ceb07d55e220b4e707c1468d
mimipeshy <perisndanu@gmail.com>
2021-07-06 15:57:36 +0300

    Finish footer styling

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,15 +5,19 @@ const Footer = () => (
   <div className="footer-container">
     <Link className="footer-link" to="/measures">
       <i className="fas fa-balance-scale-right" />
+      <span>Create Measurement</span>
     </Link>
     <Link className="footer-link" to="/">
       <i className="fas fa-bars" />
+      <span> View All </span>
     </Link>
     <Link className="footer-link" to="/progress">
       <i className="fas fa-tasks" />
+      <span>View Expenses</span>
     </Link>
     <Link className="footer-link" to="/more">
       <i className="fas fa-info" />
+      <span>Create Expense</span>
     </Link>
   </div>
 );
